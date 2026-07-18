84300773e4abe8c4357c5d6f2316977f87b97a5c
vichakan-hub <vichakan@thamai.ac.th>
2026-07-18 15:27:45 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'grade-admin-v1';
+const CACHE_VERSION = 'grade-admin-v2';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
